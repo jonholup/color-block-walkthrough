ebe7113ff0f3cfbef484ce53d5eedb223a8eba61
Jonny Holupchinski <jonholup@gmail.com>
2017-02-15 10:27:51 -0600

    adding random color prompt to page

--- client.js
+++ client.js
@@ -1,18 +1,22 @@
 $( document ).ready(function() {
 
-  var colorArray = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine']; // colors to use later
+  var colorArray = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Blue']; // colors to use later
 
-// loops through all of the colors
+  // loops through all of the colors
   for (var i = 0; i < colorArray.length; i++) {
     // adding a new block
-      var newColorBlock = $('<div>'); // create an empty div - not added to page yet
-      newColorBlock.css('background-color', colorArray[i]); // adds a color to the empty div (the ith color)
-      newColorBlock.addClass('colorBlock'); // this line adds class from css to assign height and width
-      $('#colorBlockContainer').append(newColorBlock); // Add the div to the DOM
+    var newColorBlock = $('<div>'); // create an empty div - not added to page yet
+    newColorBlock.css('background-color', colorArray[i]); // adds a color to the empty div (the ith color)
+    newColorBlock.addClass('colorBlock'); // this line adds class from css to assign height and width
+    $('#colorBlockContainer').append(newColorBlock); // Add the div to the DOM
   }
 
-
-
-
-
+  // Select a random color and add it to the DOM
+  var randomNumberSelected = randomNumber(0, colorArray.length - 1) //get random number from 0 through 4 (for now)
+  var randomColor = colorArray[randomNumberSelected]; // uses random number selected (0-4), to pick a color from array
+  $('#userColorPrompt').text(randomColor);
 });
+
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
